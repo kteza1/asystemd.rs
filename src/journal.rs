2e79c326b2ee406ae9830df524883ddab45a5667
kteza1 <k.teza1@gmail.com>
2016-03-28 14:10:34 +0530

    Adding iterator and iterator test

---
+++ src/journal.rs
@@ -0,0 +1,199 @@
+use libc::{self, free, c_int, c_char, size_t};
+use std::ffi::{CString, CStr};
+use log::{self, Log, LogRecord, LogLocation, SetLoggerError};
+use std::{fmt, ptr, result};
+use std::collections::BTreeMap;
+use ffi;
+use super::Result;
+
+/// Send preformatted fields to systemd.
+///
+/// This is a relatively low-level operation and probably not suitable unless
+/// you need precise control over which fields are sent to systemd.
+pub fn send(args: &[&str]) -> c_int {
+    let iovecs = ffi::array_to_iovecs(args);
+    unsafe { ffi::sd_journal_sendv(iovecs.as_ptr(), iovecs.len() as c_int) }
+}
+
+/// Send a simple message to systemd.
+pub fn print(lvl: u32, s: &str) -> c_int {
+    send(&[&format!("PRIORITY={}", lvl), &format!("MESSAGE={}", s)])
+}
+
+/// Send a `log::LogRecord` to systemd.
+pub fn log_record(record: &LogRecord) {
+    let lvl: usize = unsafe {
+        use std::mem;
+        mem::transmute(record.level())
+    };
+    log(lvl, record.location(), record.args());
+}
+
+pub fn log(level: usize, loc: &LogLocation, args: &fmt::Arguments) {
+    send(&[&format!("PRIORITY={}", level),
+           &format!("MESSAGE={}", args),
+           &format!("CODE_LINE={}", loc.line()),
+           &format!("CODE_FILE={}", loc.file()),
+           &format!("CODE_FUNCTION={}", loc.module_path())]);
+}
+
+pub struct JournalLog;
+impl Log for JournalLog {
+    fn enabled(&self, _metadata: &log::LogMetadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &LogRecord) {
+        log_record(record);
+    }
+}
+
+impl JournalLog {
+    pub fn init() -> result::Result<(), SetLoggerError> {
+        log::set_logger(|_max_log_level| Box::new(JournalLog))
+    }
+}
+
+pub type JournalRecord = BTreeMap<String, String>;
+
+/// A cursor into the systemd journal.
+///
+/// Supports read, next, previous, and seek operations.
+pub struct Journal {
+    j: ffi::sd_journal,
+}
+
+/// Represents the set of journal files to read.
+pub enum JournalFiles {
+    /// The system-wide journal.
+    System,
+    /// The current user's journal.
+    CurrentUser,
+    /// Both the system-wide journal and the current user's journal.
+    All,
+}
+
+impl Journal {
+    /// Open the systemd journal for reading.
+    ///
+    /// Params:
+    ///
+    /// * files: the set of journal files to read. If the calling process
+    ///   doesn't have permission to read the system journal, a call to
+    ///   `Journal::open` with `System` or `All` will succeed, but system
+    ///   journal entries won't be included. This behavior is due to systemd.
+    /// * runtime_only: if true, include only journal entries from the current
+    ///   boot. If false, include all entries.
+    /// * local_only: if true, include only journal entries originating from
+    ///   localhost. If false, include all entries.
+    pub fn open(files: JournalFiles, runtime_only: bool, local_only: bool) -> Result<Journal> {
+        let mut flags: c_int = 0;
+        if runtime_only {
+            flags |= ffi::SD_JOURNAL_RUNTIME_ONLY;
+        }
+        if local_only {
+            flags |= ffi::SD_JOURNAL_LOCAL_ONLY;
+        }
+        flags |= match files {
+            JournalFiles::System => ffi::SD_JOURNAL_SYSTEM,
+            JournalFiles::CurrentUser => ffi::SD_JOURNAL_CURRENT_USER,
+            JournalFiles::All => 0,
+        };
+
+        let journal = Journal { j: ptr::null_mut() };
+        sd_try!(ffi::sd_journal_open(&journal.j, flags));
+        sd_try!(ffi::sd_journal_seek_head(journal.j));
+        Ok(journal)
+    }
+
+    /// Read the next record from the journal. Returns `io::EndOfFile` if there
+    /// are no more records to read.
+    pub fn next_record(&self) -> Result<Option<JournalRecord>> {
+        if sd_try!(ffi::sd_journal_next(self.j)) == 0 {
+            return Ok(None);
+        }
+        unsafe { ffi::sd_journal_restart_data(self.j) }
+
+        let mut ret: JournalRecord = BTreeMap::new();
+
+        let mut sz: size_t = 0;
+        let data: *mut u8 = ptr::null_mut();
+        while sd_try!(ffi::sd_journal_enumerate_data(self.j, &data, &mut sz)) > 0 {
+            unsafe {
+                let b = ::std::slice::from_raw_parts_mut(data, sz as usize);
+                let field = ::std::str::from_utf8_unchecked(b);
+                let mut name_value = field.splitn(2, '=');
+                let name = name_value.next().unwrap();
+                let value = name_value.next().unwrap();
+                ret.insert(From::from(name), From::from(value));
+            }
+        }
+
+        Ok(Some(ret))
+    }
+
+    pub fn cursor(&self) -> Result<String> {
+        let mut c_cursor: *mut c_char = ptr::null_mut();
+        let mut cursor: String = "".to_string();
+        if sd_try!(ffi::sd_journal_get_cursor(self.j, &mut c_cursor)) == 0 {
+            unsafe {
+                // Cstr should be used for memory allocated by C
+                cursor = CStr::from_ptr(c_cursor as *const _)
+                             .to_string_lossy()
+                             .into_owned();
+
+                free(c_cursor as *mut libc::c_void);
+            }
+        }
+        Ok(cursor)
+    }
+
+    pub fn seek<S>(&self, cursor: S) -> Result<()>
+        where S: Into<String>
+    {
+        let c_position = CString::new(cursor.into());
+        // If no entry matching the specified cursor is found the call will seek to
+        // the next closest entry (in terms of time) instead
+        sd_try!(ffi::sd_journal_seek_cursor(self.j, c_position.unwrap().as_ptr() as *const _));
+        Ok(())
+    }
+}
+
+impl Iterator for Journal {
+    type Item = (JournalRecord, String);
+
+    fn next(&mut self) -> Option<(JournalRecord, String)> {
+        let next_record = self.next_record().unwrap();
+        let wait_time: u64 = 1 << 63;
+        let cursor = match self.cursor() {
+            Ok(c) => c,
+            Err(_) => return None,
+        };
+
+        match next_record {
+            Some(record) => Some((record, cursor)),
+            None => {
+                let wait_time: u64 = 1 << 63;
+                let w_ret: i32;
+                unsafe {
+                    w_ret = ffi::sd_journal_wait(self.j, wait_time);
+                }
+                if w_ret <= 0 {
+                    None
+                } else {
+                    self.next()
+                }
+            }
+        }
+    }
+}
+
+impl Drop for Journal {
+    fn drop(&mut self) {
+        if !self.j.is_null() {
+            unsafe {
+                ffi::sd_journal_close(self.j);
+            }
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+extern crate libc;
+#[macro_use] extern crate log;
+extern crate libsystemd_sys as ffi;
+pub use std::io::{Result, Error};
+
+/// An analogue of `try!()` for systemd FFI calls.
+///
+/// The parameter should be a call to a systemd FFI fn with an i32 return
+/// value. It is called, and if the return is negative then `sd_try!()`
+/// interprets it as an error code and returns IoError from the enclosing fn.
+/// Otherwise, the value of `sd_try!()` is the non-negative value returned by
+/// the FFI call.
+#[macro_export]
+macro_rules! sd_try {
+    ($e:expr) => ({
+        let ret: i32;
+        unsafe {
+            ret = $e;
+        }
+        if ret < 0 {
+            return Err($crate::Error::from_raw_os_error(-ret));
+        }
+        ret
+    })
+}
+
+/// Given an Option<&str>, either returns a pointer to a const char*, or a NULL
+/// pointer if None.
+#[macro_export]
+macro_rules! char_or_null {
+    ($e:expr) => (match $e {
+        Some(p) => ::std::ffi::CString::new(p.as_bytes()).unwrap().as_ptr() as *const ::libc::c_char,
+        None => ptr::null() as *const ::libc::c_char
+    })
+}
+
+/// High-level interface to the systemd journal.
+///
+/// The main interface for writing to the journal is `fn log()`, and the main
+/// interface for reading the journal is `struct Journal`.
+pub mod journal;
+
+/// Similar to `log!()`, except it accepts a func argument rather than hard
+/// coding `::log::log()`, and it doesn't filter on `log_enabled!()`.
+#[macro_export]
+macro_rules! log_with{
+    ($func:expr, $lvl:expr, $($arg:tt),+) => ({
+        static LOC: ::log::LogLocation = ::log::LogLocation {
+            __line: line!(),
+            __file: file!(),
+            __module_path: module_path!()
+        };
+        let lvl = $lvl;
+        $func(lvl, &LOC, &format_args!($($arg),+))
+    })
+}
+
+#[macro_export]
+macro_rules! sd_journal_log{
+    ($lvl:expr, $($arg:tt)+) => (log_with!(::systemd::journal::log, $lvl, $($arg)+))
+}
+
+/// High-level interface to the systemd daemon module.
+pub mod daemon;
